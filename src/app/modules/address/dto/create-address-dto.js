6d716d90b017bea3d90f903a4656373c16f946f2
dangphu2412 <dangphu241299@gmail.com>
2020-08-07 09:07:36 +0700

    Update seed and fix Dto create address

--- src/app/modules/address/dto/create-address-dto.js
+++ src/app/modules/address/dto/create-address-dto.js
@@ -1,6 +1,7 @@
 export default class CreateAddressDto {
     constructor(input) {
         this.userId = input.userId;
+        this.districtId = input.districtId;
         this.location = input.location;
         this.phone = input.phone;
     }
