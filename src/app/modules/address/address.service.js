87665bdbb7e80ddea99864281c7d062f8e0473be
dangphu2412 <dangphu241299@gmail.com>
2020-08-10 12:38:19 +0700

    Update response of create address

--- src/app/modules/address/address.service.js
+++ src/app/modules/address/address.service.js
@@ -1,10 +1,12 @@
 import CoreService from "../../concept/Service";
 import AddressRepository from "./address.repository";
+import DistrictRepository from "../area/repository/district.repository";
 
 class AddressService extends CoreService {
     constructor() {
         super();
         this.repository = AddressRepository;
+        this.districtRepository = DistrictRepository;
     }
 
     getAddresses(query, userId) {
@@ -18,6 +20,18 @@ class AddressService extends CoreService {
             userId,
         };
         return this.repository.getMany(query, scopes, conditions);
+    }
+
+    async create(createAddressDto) {
+        const response = await this.repository.create(createAddressDto);
+        const plain = response.get({ plain: true });
+        const { districtId } = plain;
+
+        const districtScopes = ["getCity"];
+
+        plain.district = await this.districtRepository.getByPk(districtId, districtScopes);
+
+        return plain;
     }
 }
 
