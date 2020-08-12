cfa2205fd2b148962b6a14a7b5423c8048344888
dangphu2412 <dangphu241299@gmail.com>
2020-08-12 16:04:09 +0700

    Fix router delete address

--- src/app/modules/address/address.routes.js
+++ src/app/modules/address/address.routes.js
@@ -7,6 +7,6 @@ router.get("/users/addresss", AddressHandler["getAddresses"]());
 
 router.post("/users/addresss", AddressHandler["createAddress"]());
 
-router.delete("/users/address", AddressHandler["deleteAddress"]());
+router.delete("/users/address/:id", AddressHandler["deleteAddress"]());
 
 export default router;
--- src/database/seeders/factory/crawl/index.js
+++ src/database/seeders/factory/crawl/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 const puppeteer = require("puppeteer");
 
-const electronicUrl = "https://shopee.vn/Th%E1%BB%9Di-Trang-Nam-cat.78?page=0";
+const electronicUrl = "https://nshopvn.com/";
 (async () => {
   try {
     const browser = await puppeteer.launch({ headless: true });
@@ -9,15 +9,16 @@ const electronicUrl = "https://shopee.vn/Th%E1%BB%9Di-Trang-Nam-cat.78?page=0";
     await page.goto(electronicUrl);
 
     const returnData = await page.evaluate(() => {
-      const products = document.querySelectorAll(".shopee-search-item-result__item");
+      const products = document.querySelectorAll(".products");
       const data = [];
       products.forEach((product) => {
-        const thumbnail = product.querySelector("._1gkBDw > ._3ZDC1p > img").src;
-        // const percent = product.querySelector("._2N1Tif > .percent").innerText;
+        const thumbnail = product.querySelector(".image  > img").src;
+        const content = product.querySelector(".product-title").innerText;
         // const name = product.querySelector("._3eufr2 > .O6wiAW > div").innerText;
         // const money = product.querySelector("._3eufr2 > ._2lBkmX > div._1w9jLI").innerText;
         data.push({
           thumbnail,
+          content,
         });
       });
 
